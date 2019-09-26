f7d07c17c92089cd9e474ff72e88044182a94496
LorenzoArgentieri <lorenzobass97@gmail.com>
2019-09-26 18:10:19 +0200

    clean install pkg

--- install.sh
+++ install.sh
@@ -16,15 +16,21 @@ vim-python-jedi
 EOF
 );
 
-cp .vimrc ../.vimrc;
-cp .zshrc ../.zshrc;
-if [ ! -d ../.oh-my-zsh ]
+cp .vimrc ~/.vimrc;
+cp .zshrc ~/.zshrc;
+
+#install oh my zsh
+if [ ! -d ~/.oh-my-zsh ]
 	then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
 	chsh -s $(which zsh);
 fi
 
-if [ ! ../.vim ]
-	then plug_url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs ${plug_url}
-fi	
+
+#install vim plug
+if [ ! -d ~/.vim && ! -f ~/.vim/autoload/plug.vim ]
+
+	then 
+	plug_url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs ${plug_url};
+fi
