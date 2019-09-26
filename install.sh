bcb590e0d23f9cb2148d5c9cb764e677a05cab7d
LorenzoArgentieri <lorenzobass97@gmail.com>
2019-09-26 16:18:23 +0200

    clean install pkg

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ set -e
 #install dipendencies for programs
 
 sudo apt-get -y update \
-&& sudo apt-get -y install \
-"$(cat - <<EOF 
+&& sudo apt-get -y install $(cat - <<EOF 
 curl 
 zsh
 vim
@@ -14,13 +13,14 @@ build-essential
 cmake
 python3-dev
 vim-python-jedi
-EOF)"; 
+EOF
+);
 
-cp .vimrc ../.vimrc
-cp .zshrc ../.zshrc
+cp .vimrc ../.vimrc;
+cp .zshrc ../.zshrc;
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
 chsh -s $(which zsh);
 
 sh -c "$(curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)"
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)";
