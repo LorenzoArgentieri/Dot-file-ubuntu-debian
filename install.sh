166e656dcd8d686ada2ca0f0ec4de1a180044aad
sierda <lorenzobass97@gmail.com>
2019-12-21 19:02:37 +0100

    big fixing

--- install.sh
+++ install.sh
@@ -6,26 +6,27 @@ set -e
 
 sudo apt-get -y update \
 && sudo apt-get -y install $(cat - <<EOF 
-curl 
-wget
-zsh
-vim-gtk3
 build-essential
 cmake
-python3-dev
-vim-python-jedi
+curl 
 i3
+p7zip
+p7zip-full
+p7zip-rar
+python3-dev
+ranger
 terminator
+tree
+vim-gtk3
+vim-python-jedi
+virtualbox
+wget
 zathura
 zathura-cb
 zathura-djvu
 zathura-pdf-poppler
 zathura-ps
-ranger
-virtualbox
-p7zip
-p7zip-full
-p7zip-rar
+zsh
 EOF
 );
 
@@ -37,18 +38,17 @@ if [ ! -d ~/.oh-my-zsh ]
 	then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
 	chsh -s $(which zsh);
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 #install vim plug
-if [ ! -d ~/.vim && ! -f ~/.vim/autoload/plug.vim ]
+if [ ! -f ~/.vim/autoload/plug.vim ]
 	then 
 	plug_url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs ${plug_url};
 fi
 
-source ~/.zshrc
-source ~/.vimrc
 i3-config-wizard
+
